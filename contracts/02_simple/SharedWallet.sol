bb2d58ae160d65b7b18ce81396ce1a078dca0c72
devstar0611 <devstar0611@gmail.com>
2022-05-04 02:40:21 -0700

    20220504

--- contracts/02_simple/SharedWallet.sol
+++ contracts/02_simple/SharedWallet.sol
@@ -1,32 +1,59 @@
 //SPDX-License-Identifier: MIT
+pragma solidity ^0.6.1;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/math/SafeMath.sol";
 
-pragma solidity ^0.8.1;
+contract Allowance  is Ownable {
 
-contract SharedWallet is Ownable {
-
-    address owner;
+    using SafeMath for uint:
+    
+    event AllowanceChanged(address indexed _forWho, 
+                            address indexed _byWhom, 
+                            uint _oldAmount, 
+                            uint _newAmount);
     
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
     }
 
     mapping(address => uint) public allowance;
-
-    function addAllowance(address _who, uint _amount) public isOwner {
-        allowance[_who] == _amount;
-    }
     
+    function setAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] = 
+    }
+
+    function reduceAllowance(address _who, uint _amount) public onlyOwner {
+        allowance[_who] -= _amount;
+    }
+
+
     modifier ownerOrAllowed(uint _amount) {
-        
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed!");
+        _;
     }
+}
 
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
-        _to.transfer(_amount);
+contract SharedWallet is Allowance {
     
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
+
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "Contract doesn't own enough money");
+        if(!isOwner()) {
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit MoneySent(_to, _amount);
+        _to.transfer(_amount);
     }
     
     receive() external payable {
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
