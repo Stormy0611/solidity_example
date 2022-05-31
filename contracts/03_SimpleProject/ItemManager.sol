f7799ffed6a87867e14f8b930945c5125352cce9
devstar0611 <devstar0611@gmail.com>
2022-05-31 04:39:18 -0700

    X

--- contracts/03_SimpleProject/ItemManager.sol
+++ contracts/03_SimpleProject/ItemManager.sol
@@ -2,25 +2,28 @@ pragma solidity ^0.6.0;
 contract ItemManager{
  enum SupplyChainSteps{Created, Paid, Delivered}
  struct S_Item {
+    Item _item;
     ItemManager.SupplyChainSteps _step;
     string _identifier;
-    uint _priceInWei;
  }
  mapping(uint => S_Item) public items;
  uint index;
- event SupplyChainStep(uint _itemIndex, uint _step);
+ event SupplyChainStep(uint _itemIndex, uint _step, address _address);
  function createItem(string memory _identifier, uint _priceInWei) public {
-    items[index]._priceInWei = _priceInWei;
+    Item item = new Item(this, _priceInWei, index);
+    items[index]._item = item;
     items[index]._step = SupplyChainSteps.Created;
     items[index]._identifier = _identifier;
-    emit SupplyChainStep(index, uint(items[index]._step));
+    emit SupplyChainStep(index, uint(items[index]._step), address(item));
     index++;
  }
  function triggerPayment(uint _index) public payable {
-    require(items[_index]._priceInWei <= msg.value, "Not fully paid");
+    Item item = items[_index]._item;
+    require(address(item) == msg.sender, "Only items are allowed to update themselves");
+    require(item.priceInWei() == msg.value, "Not fully paid yet");
     require(items[_index]._step == SupplyChainSteps.Created, "Item is further in the supply chain");
     items[_index]._step = SupplyChainSteps.Paid;
-    emit SupplyChainStep(_index, uint(items[_index]._step));
+    emit SupplyChainStep(_index, uint(items[_index]._step), address(item));
  }
  function triggerDelivery(uint _index) public {
     require(items[_index]._step == SupplyChainSteps.Paid, "Item is further in the supply chain");
